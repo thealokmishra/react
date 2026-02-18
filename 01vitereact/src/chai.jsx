e12522f262d128ef791f4ce14bd6e0a2cb329c06
Alok Mishra <91321828+thealokmishra@users.noreply.github.com>
2026-02-18 17:36:32 +0530

    how react works internally

---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,12 @@
+// first letter of fn should be capitalized in React component,
+// you can only return one element from a React component, but that element can have as many children as you want so we wrap into div if returning multiple elements
+//  '<></>' is a React Fragment, it allows us to return multiple elements without adding an extra node to the DOM
+function Chai() {
+  return (
+    <>
+      <h1>Chai aur React</h1>
+    </>
+  );
+}
+
+export default Chai;
